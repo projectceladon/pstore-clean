6efa2a140270452900a06667a59a23b4756e1cbf
Qin, Xiaokang <xiaokang.qin@intel.com>
2016-11-22 20:52:58 +0800

    Add the missing liblog to pass the build
    Change-Id: Iae87bfa16c78a08086c861ed34bdbb8b737dc4a8
    Signed-off-by: Qin, Xiaokang <xiaokang.qin@intel.com>

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wall -Wextra -Werror
 LOCAL_SRC_FILES := pstore-clean.c
-LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE := pstore-clean
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := intel
